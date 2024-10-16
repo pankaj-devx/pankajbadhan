59387d0c8ffde61ae9b29b2d6daab1a9b03c3621
king_badhan <pankaj4utanda@gmail.com>
2024-10-16 17:55:43 +0530

    nav_com

---
+++ app.js
@@ -0,0 +1,24 @@
+const themeToggle = document.getElementById('themeToggle'); 
+const body = document.body;
+
+let isDarkTheme = localStorage.getItem('theme') === 'dark';
+
+if (isDarkTheme) {
+  body.classList.add('dark-theme');
+} else {
+  body.classList.add('light-theme');
+}
+
+themeToggle.addEventListener('click', function () {
+  isDarkTheme = !isDarkTheme;
+
+  if (isDarkTheme) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light');
+  }
+});

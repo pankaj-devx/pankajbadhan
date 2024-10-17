daf27c540b4a082e64826e290dda01d3003d7082
king_badhan <pankaj4utanda@gmail.com>
2024-10-17 09:03:16 +0530

    com_nav

---
+++ abc.c
@@ -0,0 +1,7 @@
+// Conditional Instructions:  if,  if-else,  switch
+
+#include <stdio.h>
+int main() {
+  int num = 6;
+  if(num == 5) printf("Yes number is 5");
+}
--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const themeToggle = document.getElementById('themeToggle'); 
 const body = document.body;
 
 let isDarkTheme = localStorage.getItem('theme') === 'dark';
 
-if (isDarkTheme) {
-  body.classList.add('dark-theme');
-} else {
-  body.classList.add('light-theme');
-}
+if (isDarkTheme) body.classList.add('dark-theme');
+else             body.classList.add('light-theme');
 
 themeToggle.addEventListener('click', function () {
   isDarkTheme = !isDarkTheme;
-
   if (isDarkTheme) {
     body.classList.remove('light-theme');
     body.classList.add('dark-theme');
     localStorage.setItem('theme', 'dark');
+    moon.classList.remove('dsn');
+    sun.classList.add('dsn');
   } else {
     body.classList.remove('dark-theme');
     body.classList.add('light-theme');
     localStorage.setItem('theme', 'light');
+    sun.classList.remove('dsn');
+    moon.classList.add('dsn');
   }
 });
